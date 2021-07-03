df925333db2459a681f0c1a355de922469d3ecc5
Hiko-Tho <tho.n@hikotech.io>
2021-07-03 16:42:12 +0700

    3/7

--- api/shared/security/encodeDecode.security.js
+++ api/shared/security/encodeDecode.security.js
@@ -1,11 +1,29 @@
-const crypto = require('crypto-js');
+const crypto = require('crypto');
+const algorithm = 'aes-192-cbc';
 
 const encode = async (content) => {
-    return await crypto.AES.encrypt(content, process.env.encrypt_key).toString();
+	try {
+		const iv = crypto.randomBytes(16)
+		const cipher = crypto.createCipheriv(algorithm, Buffer.from(process.env.encrypt_key, 'utf8'), iv)
+		const encrypt = cipher.update(content)
+		const encrypted = Buffer.concat([encrypt, cipher.final()])
+		return encrypted.toString('hex')+"-"+iv.toString('hex')
+	} catch (error) {
+		return null
+	}
 }
 
-const decode = async (content) => {
-    return await crypto.AES.decrypt(content, process.env.encrypt_key).toString(crypto.enc.Utf8);
+const decode = async (hash) => {
+    try {
+			const content = hash.split('-')
+			const iv = Buffer.from(content[1], 'hex')
+			const encrypted = Buffer.from(content[0], 'hex')
+			const decipher = crypto.createDecipheriv(algorithm, Buffer.from(process.env.encrypt_key, 'utf8'), iv)
+			const decrypt = decipher.update(encrypted)
+			return Buffer.from([decrypt, decipher.final()]).toString()
+		} catch (error) {
+			return null
+		}
 }
 
 module.exports = {
